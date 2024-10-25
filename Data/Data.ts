b211a26e23f69a3a3a939079a699afc8d12e1ee0
Ismail Ahmed <aismail@kictl.com>
2024-10-25 17:40:14 +0500

    code updated

--- Data/Data.ts
+++ Data/Data.ts
@@ -15,239 +15,239 @@ export const aboutInfo = {
   project: "100+",
   website: "40+",
 };
-  
-  export const servicesData = [
-    {
-      id: 1,
-      title: " Web Applications",
-      description: "Tailored web apps built with the latest technologies.",
-      icon: "/images/s1.png",
-    },
-    {
-      id: 2,
-      title: "E-commerce Websites",
-      description:
-        "Secure online stores with user-friendly shopping and payments.",
-      icon: "/images/s2.png",
-    },
-    {
-      id: 3,
-      title: "API Integration",
-      description:
-        "Seamless integration for smooth data sharing and functionality.",
-      icon: "/images/s3.png",
-    },
-    {
-      id: 4,
-      title: "User Interfaces",
-      description:
-        "Engaging, responsive front-end designs for better user experiences.",
-      icon: "/images/s4.png",
-    },
-    {
-      id: 5,
-      title: "Database Solutions",
-      description:
-        "Efficient and scalable database management for your data needs.",
-      icon: "/images/s5.png",
-    },
-    {
-      id: 6,
-      title: "Real-Time Features",
-      description: "Add live chat, notifications, and real-time data updates.",
-      icon: "/images/s6.png",
-    },
-    {
-      id: 7,
-      title: "Speed Optimization",
-      description: "Boost performance with faster load times and optimization.",
-      icon: "/images/s7.png",
-    },
-    {
-      id: 8,
-      title: "Website Management",
-      description: "Easy content management systems with flexible controls.",
-      icon: "/images/s8.png",
-    },
-  ];
-  
-  export const projectData = [
-    {
-      id: 1,
-      image: "/images/hello-coders.png",
-      url: "https://hello-coders-coding-school.vercel.app/",
-    },
-    {
-      id: 2,
-      image: "/images/HTML-Portfolio.png",
-      url: "https://portfolio-html-css-psi-murex.vercel.app/#/",
-    },
-    {
-      id: 3,
-      image: "/images/gov-sindh-clone.png",
-      url: "https://clone-governorsindh-com.vercel.app//",
-    },
-    {
-      id: 4,
-      image: "/images/snack-game.png",
-      url: "https://snake-game-30-days-of30-projects-next-js.vercel.app//",
-    },
-    {
-      id: 5,
-      image: "/images/p30.png",
-      url: "https://audio-player-app-30-days-of-30-projects-next-js.vercel.app/",
-    },
-    {
-      id: 6,
-      image: "/images/p29.png",
-      url: "https://random-user-generator-30-days-of30-projects-next-js.vercel.app//",
-    },
-    {
-      id: 7,
-      image: "/images/p28.png",
-      url: "https://todo-list-30-days-of30-projects-next-js.vercel.app/",
-    },
-    {
-      id: 8,
-      image: "/images/p27.png",
-      url: "https://url-shortner-30-days-of30-projects-next-js.vercel.app/",
-    },
-    {
-      id: 9,
-      image: "/images/weatherapp.png",
-      url: "https://weather-widget-30-daysof-30-projects.vercel.app/",
-    },
-  ];
-  
-  export const skillsData = [
-    {
-      id: 1,
-      title: "HTML",
-      image: "/images/html.svg",
-      percent: "80%",
-    },
-    {
-      id: 2,
-      title: "CSS",
-      image: "/images/css.svg",
-      percent: "80%",
-    },
-    {
-      id: 3,
-      title: "JavaScript",
-      image: "/images/js.svg",
-      percent: "80%",
-    },
-    {
-      id: 4,
-      title: "TypeScript",
-      image: "/images/ts.svg",
-      percent: "80%",
-    },
-    {
-      id: 5,
-      title: "React",
-      image: "/images/react.svg",
-      percent: "70%",
-    },
-    {
-      id: 6,
-      title: "Node JS",
-      image: "/images/node.svg",
-      percent: "60%",
-    },
-    {
-      id: 7,
-      title: "MongoDB",
-      image: "/images/mongo.svg",
-      percent: "65%",
-    },
-    {
-      id: 8,
-      title: "Python",
-      image: "/images/python.svg",
-      percent: "55%",
-    },
-    {
-      id: 9,
-      title: "Firebase",
-      image: "/images/firebase.svg",
-      percent: "75%",
-    },
-    {
-      id: 10,
-      title: "Vue",
-      image: "/images/vue.svg",
-      percent: "35%",
-    },
-  ];
-  
-  export const clientReviews = [
-    {
-      name: "John Doe",
-      review:
-        "Exceptional service! The team delivered exactly what we needed on time. Very professional",
-      rating: 5,
-      profession: "Marketing Manager",
-      image: "/images/u1.jpg",
-    },
-    {
-      name: "Jane Smith",
-      review:
-        "Highly skilled developers who understand the client&apos;s vision and execute perfectly.",
-      rating: 4.8,
-      profession: "Business Owner",
-      image: "/images/u2.jpg",
-    },
-    {
-      name: "Michael Johnson",
-      review:
-        "Great communication and excellent results. Our new site performs beautifully.",
-      rating: 4.6,
-      profession: "Project Coordinator",
-      image: "/images/u3.jpg",
-    },
-    {
-      name: "Emily Brown",
-      review:
-        "Professional, efficient, and highly knowledgeable. Would definitely recommend their services!",
-      rating: 4.9,
-      profession: "Creative Director",
-      image: "/images/u4.jpg",
-    },
-  ];
-  
-  export const blogs = [
-    {
-      id: 1,
-      title: "Top Web Development Trends to Watch in 2024",
-      summary:
-        "Explore the key web development trends for 2024 and their impact on your business.",
-      date: "August 18, 2024",
-      image: "/images/b1.jpg",
-    },
-    {
-      id: 2,
-      title: "Why Your Business Needs a Custom Web Application",
-      summary:
-        "Discover why custom web applications are crucial for driving business growth and efficiency.",
-      date: "August 10, 2024",
-      image: "/images/b2.jpg",
-    },
-    {
-      id: 3,
-      title: "The Importance of Website Speed Optimization",
-      summary:
-        "Understand how website speed impacts user experience and learn essential optimization tips.",
-      date: "July 25, 2024",
-      image: "/images/b3.jpg",
-    },
-  ];
-  
-  export const contactData = {
-    phone: "+92(332-2241405)",
-    whatsapp: "+92(332-2241405)",
-    email: "ismailahmedshahpk@gmail.com",
-    linkedin: "https://www.linkedin.com/in/ismail-ahmed-shah-2455b01ba/",
-    address: "Karachi , Pakistan",
-  };
+
+export const servicesData = [
+  {
+    id: 1,
+    title: " Web Applications",
+    description: "Tailored web apps built with the latest technologies.",
+    icon: "/images/s1.png",
+  },
+  {
+    id: 2,
+    title: "E-commerce Websites",
+    description:
+      "Secure online stores with user-friendly shopping and payments.",
+    icon: "/images/s2.png",
+  },
+  {
+    id: 3,
+    title: "API Integration",
+    description:
+      "Seamless integration for smooth data sharing and functionality.",
+    icon: "/images/s3.png",
+  },
+  {
+    id: 4,
+    title: "User Interfaces",
+    description:
+      "Engaging, responsive front-end designs for better user experiences.",
+    icon: "/images/s4.png",
+  },
+  {
+    id: 5,
+    title: "Database Solutions",
+    description:
+      "Efficient and scalable database management for your data needs.",
+    icon: "/images/s5.png",
+  },
+  {
+    id: 6,
+    title: "Real-Time Features",
+    description: "Add live chat, notifications, and real-time data updates.",
+    icon: "/images/s6.png",
+  },
+  {
+    id: 7,
+    title: "Speed Optimization",
+    description: "Boost performance with faster load times and optimization.",
+    icon: "/images/s7.png",
+  },
+  {
+    id: 8,
+    title: "Website Management",
+    description: "Easy content management systems with flexible controls.",
+    icon: "/images/s8.png",
+  },
+];
+
+export const projectData = [
+  {
+    id: 1,
+    image: "/images/hello-coders.png",
+    url: "https://hello-coders-coding-school.vercel.app/",
+  },
+  {
+    id: 2,
+    image: "/images/HTML-Portfolio.png",
+    url: "https://portfolio-html-css-psi-murex.vercel.app/#/",
+  },
+  {
+    id: 3,
+    image: "/images/gov-sindh-clone.png",
+    url: "https://clone-governorsindh-com.vercel.app//",
+  },
+  {
+    id: 4,
+    image: "/images/snack-game.png",
+    url: "https://snake-game-30-days-of30-projects-next-js.vercel.app//",
+  },
+  {
+    id: 5,
+    image: "/images/p30.png",
+    url: "https://audio-player-app-30-days-of-30-projects-next-js.vercel.app/",
+  },
+  {
+    id: 6,
+    image: "/images/p29.png",
+    url: "https://random-user-generator-30-days-of30-projects-next-js.vercel.app//",
+  },
+  {
+    id: 7,
+    image: "/images/p28.png",
+    url: "https://todo-list-30-days-of30-projects-next-js.vercel.app/",
+  },
+  {
+    id: 8,
+    image: "/images/p27.png",
+    url: "https://url-shortner-30-days-of30-projects-next-js.vercel.app/",
+  },
+  {
+    id: 9,
+    image: "/images/weatherapp.png",
+    url: "https://weather-widget-30-daysof-30-projects.vercel.app/",
+  },
+];
+
+export const skillsData = [
+  {
+    id: 1,
+    title: "HTML",
+    image: "/images/html.svg",
+    percent: "80%",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    image: "/images/css.svg",
+    percent: "80%",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    image: "/images/js.svg",
+    percent: "80%",
+  },
+  {
+    id: 4,
+    title: "TypeScript",
+    image: "/images/ts.svg",
+    percent: "80%",
+  },
+  {
+    id: 5,
+    title: "React",
+    image: "/images/react.svg",
+    percent: "70%",
+  },
+  {
+    id: 6,
+    title: "Node JS",
+    image: "/images/node.svg",
+    percent: "60%",
+  },
+  {
+    id: 7,
+    title: "MongoDB",
+    image: "/images/mongo.svg",
+    percent: "65%",
+  },
+  {
+    id: 8,
+    title: "Python",
+    image: "/images/python.svg",
+    percent: "55%",
+  },
+  {
+    id: 9,
+    title: "Firebase",
+    image: "/images/firebase.svg",
+    percent: "75%",
+  },
+  {
+    id: 10,
+    title: "Vue",
+    image: "/images/vue.svg",
+    percent: "35%",
+  },
+];
+
+export const clientReviews = [
+  {
+    name: "John Doe",
+    review:
+      "Exceptional service! The team delivered exactly what we needed on time. Very professional",
+    rating: 5,
+    profession: "Marketing Manager",
+    image: "/images/u1.jpg",
+  },
+  {
+    name: "Jane Smith",
+    review:
+      "Highly skilled developers who understand the client&apos;s vision and execute perfectly.",
+    rating: 4.8,
+    profession: "Business Owner",
+    image: "/images/u2.jpg",
+  },
+  {
+    name: "Michael Johnson",
+    review:
+      "Great communication and excellent results. Our new site performs beautifully.",
+    rating: 4.6,
+    profession: "Project Coordinator",
+    image: "/images/u3.jpg",
+  },
+  {
+    name: "Emily Brown",
+    review:
+      "Professional, efficient, and highly knowledgeable. Would definitely recommend their services!",
+    rating: 4.9,
+    profession: "Creative Director",
+    image: "/images/u4.jpg",
+  },
+];
+
+export const blogs = [
+  {
+    id: 1,
+    title: "Top Web Development Trends to Watch in 2024",
+    summary:
+      "Explore the key web development trends for 2024 and their impact on your business.",
+    date: "August 18, 2024",
+    image: "/images/b1.jpg",
+  },
+  {
+    id: 2,
+    title: "Why Your Business Needs a Custom Web Application",
+    summary:
+      "Discover why custom web applications are crucial for driving business growth and efficiency.",
+    date: "August 10, 2024",
+    image: "/images/b2.jpg",
+  },
+  {
+    id: 3,
+    title: "The Importance of Website Speed Optimization",
+    summary:
+      "Understand how website speed impacts user experience and learn essential optimization tips.",
+    date: "July 25, 2024",
+    image: "/images/b3.jpg",
+  },
+];
+
+export const contactData = {
+  phone: "+92(332-2241405)",
+  whatsapp: "+92(332-2241405)",
+  email: "ismailahmedshahpk@gmail.com",
+  linkedin: "https://www.linkedin.com/in/ismail-ahmed-shah-2455b01ba/",
+  address: "Karachi , Pakistan",
+};
