73b3a0bc28b5603e140f218085d76e7a83a200eb
Ismail Ahmed <aismail@kictl.com>
2025-02-15 06:09:38 +0500

    debug error

--- app/Explore/page.tsx
+++ app/Explore/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import coursesData from '@/data/projects.json';
+import projects from '../../Data/projects.json';
 import Image from 'next/image';
 
 const ExploreMore = () => {
@@ -8,9 +8,9 @@ const ExploreMore = () => {
     <div className='w-full p-10 mt-1 bg-gray-600'>
       <h1 className='text-3xl font-bold text-center mb-6 mt-24 text-white'>My Projects</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        {coursesData.projects.map((project) => (
+        {projects.projects.map((project) => (
           <div key={project.id} className='bg-gray-800 text-white p-6 rounded-lg shadow-lg'>
-            <Image src={project.image} alt={project.title} className='w-full h-40 object-cover mb-4 rounded-md' />
+            <Image width={300} height={300} src={project.image} alt={project.title} className='w-full h-40 object-cover mb-4 rounded-md' />
             <h2 className='text-2xl font-bold mb-2'>{project.title}</h2>
             <p className='mb-4'>{project.description}</p>
             <p className='text-xl font-semibold'>Launched on: {project.date}</p>
